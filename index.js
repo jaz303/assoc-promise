091881b775f2f4236dfdbae3a88696ce17beca51
Jason Frame <jason@onehackoranother.com>
2015-06-12 11:58:48 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 module.exports = function(obj) {
-	var remain = Object.keys(obj).length;
-	if (remain === 0) {
-		return Promise.resolve({});
-	}
-	return new Promise(function(resolve, reject) {
-		var done = false, res = {};
-		function yes(k, val) {
-			if (done) return;
-			res[k] = val;
-			if (--remain === 0) {
-				done = true;
-				resolve(res);
-			}
-		}
-		function no(err) {
-			if (done) return;
-			done = true;
-			reject(err);
-		}
-		for (var k in obj) {
-			if (obj[k] && typeof obj[k].then === 'function') {
-				obj[k].then(yes.bind(this, k), no);	
-			} else {
-				yes(k, obj[k]);
-			}
-		}
-	});
+    var remain = Object.keys(obj).length;
+    if (remain === 0) {
+        return Promise.resolve({});
+    }
+    return new Promise(function(resolve, reject) {
+        var done = false, res = {};
+        function yes(k, val) {
+            if (done) return;
+            res[k] = val;
+            if (--remain === 0) {
+                done = true;
+                resolve(res);
+            }
+        }
+        function no(err) {
+            if (done) return;
+            done = true;
+            reject(err);
+        }
+        for (var k in obj) {
+            if (obj[k] && typeof obj[k].then === 'function') {
+                obj[k].then(yes.bind(this, k), no); 
+            } else {
+                yes(k, obj[k]);
+            }
+        }
+    });
 }
